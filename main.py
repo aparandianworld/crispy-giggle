dc287e179ae8b1b1a2f31d402225ca6d23473277
Aaron Parandian <aparandian@apple.com>
2025-01-19 19:07:15 -0800

    003. updates extract_text to use context manager and adds question and response block

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ from langchain.prompts import PromptTemplate
 
 def extract_text_from_pdf(pdf_path) -> str:
     try:
-        doc = fitz.open(pdf_path)
-        text = ""
-        for page in doc:
-            text += page.get_text()
-        return text
+        with fitz.open(pdf_path) as doc:
+            text = ""
+            for page in doc:
+                text += page.get_text()
+            return text
     except Exception as e:
         return f"Error extracting text from {pdf_path}: {e}"
-    finally:
-        doc.close()
 
 def main():
-    model_name = "gpt-3.5-turbo"
+    model_name = "gpt-4"
     temperature = 0.5
 
     if "OPENAI_API_KEY" not in os.environ:
@@ -31,10 +29,12 @@ def main():
                 print("Exiting program...")
                 return
 
-            prompt = extract_text_from_pdf(pdf_path).strip()
-            assistant_template = prompt + """
+            extracted_text = extract_text_from_pdf(pdf_path).strip()
+            assistant_template = f"""
+            {extracted_text}
             You are an experienced hotel manager and provide assistance regarding general information about the hotel. Please do not provide any information outside of the context of the hotel.
-            Question: {question}
+
+            Question: {{question}}
             Answer:
             """
 
@@ -45,6 +45,15 @@ def main():
             
             llm = OpenAI(model = model_name, temperature = temperature)
             llm_chain = assistant_prompt_template | llm
+
+            while True:
+                question = input("Ask a question about the hotel or `quit` to exit the program: ")
+                if question.lower() == "quit":
+                    print("Exiting program...")
+                    break
+                response = llm_chain.invoke({"question": question})
+                if response:
+                    print(f"Answer: {response}")
 
     except Exception as e:
         print(f"Error: {e}")
